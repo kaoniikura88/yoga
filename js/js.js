f222c2f27826bcb4c36aab14197bc0195df8950d
kaoniikura88 <sapozhnikova_anastasia@mail.ru>
2020-03-03 20:29:39 +0300

    new

--- js/js.js
+++ js/js.js
@@ -168,3 +168,24 @@ const item = new Options(300, 350, "red", 14, "center");
 
 item.createDiv();
 
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+
+            inputUsd.value = inputRub.value / data.usd;
+        } else {
+            inputUsd.value = "Что-то пошло не так!";
+        }
+    });
+
+});
